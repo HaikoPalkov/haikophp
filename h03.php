22ecb57f70d88012d1f67bdde032c9544316a14b
Haiko Palkov <hpalkov@hkhk.edu.ee>
2026-04-22 13:44:13 +0300

    new file:   h03.php

---
+++ h03.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>06 - PHP - Vormid</title>
+</head>
+<body>
+    <h1>Harjutus 03</h1>
+    <h2>Trapetsi pindala</h2>
+    <form action="" method="get">
+        a: <input type="number" name="a"><br>
+        b: <input type="number" name="b"><br>
+        h: <input type="number" name="h"><br>
+        <input type="submit" value="Arvuta">
+    </form>
+    <?php
+        if (!empty($_GET)) {
+    $a = $_GET['a'];
+    $b = $_GET['b'];
+    $h = $_GET['h'];
+    $s = (($a+$b)/2) * $h;
+    echo "Trapetsi pindala on: ".$s."<br>";
+        }
+    ?>
+</body>
+</html>
